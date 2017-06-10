1c1dfbd80d873ca2b4ba9de470982e1d691cec2a
Karolis Rusenas <karolis.rusenas@gmail.com>
2017-06-10 15:05:20 +0100

    version tests

--- util/version/version_test.go
+++ util/version/version_test.go
@@ -56,6 +56,16 @@ func TestShouldUpdate(t *testing.T) {
 		wantErr bool
 	}{
 		{
+			name: "new lower, policy all",
+			args: args{
+				current: &types.Version{Major: 1, Minor: 4, Patch: 5},
+				new:     &types.Version{Major: 1, Minor: 4, Patch: 3},
+				policy:  types.PolicyTypeAll,
+			},
+			want:    false,
+			wantErr: false,
+		},
+		{
 			name: "new minor increase, policy all",
 			args: args{
 				current: &types.Version{Major: 1, Minor: 4, Patch: 5},
@@ -73,6 +83,66 @@ func TestShouldUpdate(t *testing.T) {
 				policy:  types.PolicyTypeAll,
 			},
 			want:    false,
+			wantErr: false,
+		},
+		{
+			name: "minor increase, policy major",
+			args: args{
+				current: &types.Version{Major: 1, Minor: 4, Patch: 5},
+				new:     &types.Version{Major: 1, Minor: 5, Patch: 5},
+				policy:  types.PolicyTypeMajor,
+			},
+			want:    false,
+			wantErr: false,
+		},
+		{
+			name: "major increase, policy major",
+			args: args{
+				current: &types.Version{Major: 1, Minor: 4, Patch: 5},
+				new:     &types.Version{Major: 2, Minor: 4, Patch: 5},
+				policy:  types.PolicyTypeMajor,
+			},
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name: "patch increase, policy patch",
+			args: args{
+				current: &types.Version{Major: 1, Minor: 4, Patch: 5},
+				new:     &types.Version{Major: 1, Minor: 4, Patch: 6},
+				policy:  types.PolicyTypePatch,
+			},
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name: "patch AND major increase, policy patch",
+			args: args{
+				current: &types.Version{Major: 1, Minor: 4, Patch: 5},
+				new:     &types.Version{Major: 2, Minor: 4, Patch: 6},
+				policy:  types.PolicyTypePatch,
+			},
+			want:    false,
+			wantErr: false,
+		},
+		{
+			name: "patch same, policy patch",
+			args: args{
+				current: &types.Version{Major: 1, Minor: 4, Patch: 5},
+				new:     &types.Version{Major: 1, Minor: 4, Patch: 5},
+				policy:  types.PolicyTypePatch,
+			},
+			want:    false,
+			wantErr: false,
+		},
+		{
+			name: "minor increase, policy minor",
+			args: args{
+				current: &types.Version{Major: 1, Minor: 4, Patch: 5},
+				new:     &types.Version{Major: 1, Minor: 5, Patch: 5},
+				policy:  types.PolicyTypeMinor,
+			},
+			want:    true,
 			wantErr: false,
 		},
 	}
