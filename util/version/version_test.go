76d3a090c9f4cc17be6f29c8192bdbddf805bda5
Karolis Rusenas <karolis.rusenas@gmail.com>
2017-06-10 12:57:24 +0100

    tests for version update

--- util/version/version_test.go
+++ util/version/version_test.go
@@ -23,6 +23,11 @@ func TestGetVersionFromImageName(t *testing.T) {
 			want:    &types.Version{Major: 1, Minor: 4, Patch: 5},
 			wantErr: false,
 		},
+		{
+			name:    "image latest",
+			args:    args{name: "karolis/webhook-demo:latest"},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -33,6 +38,53 @@ func TestGetVersionFromImageName(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("GetVersionFromImageName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestShouldUpdate(t *testing.T) {
+	type args struct {
+		current *types.Version
+		new     *types.Version
+		policy  types.PolicyType
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		{
+			name: "new minor increase, policy all",
+			args: args{
+				current: &types.Version{Major: 1, Minor: 4, Patch: 5},
+				new:     &types.Version{Major: 1, Minor: 4, Patch: 6},
+				policy:  types.PolicyTypeAll,
+			},
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name: "no increase, policy all",
+			args: args{
+				current: &types.Version{Major: 1, Minor: 4, Patch: 5},
+				new:     &types.Version{Major: 1, Minor: 4, Patch: 5},
+				policy:  types.PolicyTypeAll,
+			},
+			want:    false,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ShouldUpdate(tt.args.current, tt.args.new, tt.args.policy)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ShouldUpdate() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("ShouldUpdate() = %v, want %v", got, tt.want)
 			}
 		})
 	}
