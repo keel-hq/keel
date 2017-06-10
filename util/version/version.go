459bef6a15692a2669846288713ad5bd3f00ad7a
Karolis Rusenas <karolis.rusenas@gmail.com>
2017-06-10 15:05:12 +0100

    should update checker

--- util/version/version.go
+++ util/version/version.go
@@ -3,6 +3,7 @@ package version
 import (
 	// "strconv"
 	"errors"
+	"fmt"
 	"strings"
 
 	"github.com/rusenask/keel/types"
@@ -37,4 +38,33 @@ func GetVersionFromImageName(name string) (*types.Version, error) {
 	}
 
 	return nil, ErrVersionTagMissing
+}
+
+// ShouldUpdate - checks whether update is needed
+func ShouldUpdate(current *types.Version, new *types.Version, policy types.PolicyType) (bool, error) {
+	currentVersion, err := semver.NewVersion(current.String())
+	if err != nil {
+		return false, fmt.Errorf("failed to parse current version: %s", err)
+	}
+	newVersion, err := semver.NewVersion(new.String())
+	if err != nil {
+		return false, fmt.Errorf("failed to parse new version: %s", err)
+	}
+
+	// new version is not higher than current - do nothing
+	if !currentVersion.LessThan(*newVersion) {
+		return false, nil
+	}
+
+	switch policy {
+	case types.PolicyTypeAll:
+		return true, nil
+	case types.PolicyTypeMajor:
+		return newVersion.Major > currentVersion.Major, nil
+	case types.PolicyTypeMinor:
+		return newVersion.Major == currentVersion.Major && newVersion.Minor > currentVersion.Minor, nil
+	case types.PolicyTypePatch:
+		return newVersion.Major == currentVersion.Major && newVersion.Minor == currentVersion.Minor && newVersion.Patch > currentVersion.Patch, nil
+	}
+	return false, nil
 }
