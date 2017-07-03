3d8eb41d4a4425c1447657b34fc6911c1a48f0c0
Karolis Rusenas <karolis.rusenas@gmail.com>
2017-07-03 22:22:51 +0100

    ignore version bump if version is the same

--- provider/kubernetes/versioned_updates_test.go
+++ provider/kubernetes/versioned_updates_test.go
@@ -91,6 +91,56 @@ func TestProvider_checkVersionedDeployment(t *testing.T) {
 			wantErr:                    false,
 		},
 		{
+			name: "standard ignore version bump",
+			args: args{
+				newVersion: unsafeGetVersion("1.1.1"),
+				policy:     types.PolicyTypeAll,
+				repo:       &types.Repository{Name: "gcr.io/v2-namespace/hello-world", Tag: "1.1.1"},
+				deployment: v1beta1.Deployment{
+					meta_v1.TypeMeta{},
+					meta_v1.ObjectMeta{
+						Name:      "dep-1",
+						Namespace: "xxxx",
+						Labels:    map[string]string{types.KeelPolicyLabel: "all"},
+					},
+					v1beta1.DeploymentSpec{
+						Template: v1.PodTemplateSpec{
+							Spec: v1.PodSpec{
+								Containers: []v1.Container{
+									v1.Container{
+										Image: "gcr.io/v2-namespace/hello-world:1.1.1",
+									},
+								},
+							},
+						},
+					},
+					v1beta1.DeploymentStatus{},
+				},
+			},
+			wantUpdated: v1beta1.Deployment{
+				meta_v1.TypeMeta{},
+				meta_v1.ObjectMeta{
+					Name:      "dep-1",
+					Namespace: "xxxx",
+					Labels:    map[string]string{types.KeelPolicyLabel: "all"},
+				},
+				v1beta1.DeploymentSpec{
+					Template: v1.PodTemplateSpec{
+						Spec: v1.PodSpec{
+							Containers: []v1.Container{
+								v1.Container{
+									Image: "gcr.io/v2-namespace/hello-world:1.1.1",
+								},
+							},
+						},
+					},
+				},
+				v1beta1.DeploymentStatus{},
+			},
+			wantShouldUpdateDeployment: false,
+			wantErr:                    false,
+		},
+		{
 			name: "multiple containers, version bump one",
 			args: args{
 				newVersion: unsafeGetVersion("1.1.2"),
