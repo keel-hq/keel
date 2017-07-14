4269d8b14e6238f91d05f839ea6b270b2c1d3f3d
Karolis Rusenas <karolis.rusenas@gmail.com>
2017-07-14 21:42:07 +0100

    test

---
+++ provider/helm/implementer_test.go
@@ -0,0 +1,18 @@
+package helm
+
+import (
+	"testing"
+)
+
+func TestImplementerList(t *testing.T) {
+	imp := NewHelmImplementer("192.168.99.100:30083")
+	releases, err := imp.ListReleases()
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	if releases.Count == 0 {
+		t.Errorf("why no releases? ")
+	}
+
+}
