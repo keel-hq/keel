779cbbcae9e580575fce3bdeeb196dc2261043b0
Karolis Rusenas <karolis.rusenas@gmail.com>
2017-07-20 20:44:27 +0100

    first draft of unversioned updates

--- provider/helm/unversioned_updates.go
+++ provider/helm/unversioned_updates.go
@@ -1,1 +1,85 @@
 package helm
+
+import (
+	"fmt"
+
+	"github.com/rusenask/keel/types"
+	"github.com/rusenask/keel/util/image"
+
+	hapi_chart "k8s.io/helm/pkg/proto/hapi/chart"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+func checkUnversionedRelease(repo *types.Repository, namespace, name string, chart *hapi_chart.Chart, config *hapi_chart.Config) (plan *UpdatePlan, shouldUpdateRelease bool, err error) {
+
+	plan = &UpdatePlan{
+		Chart:     chart,
+		Namespace: namespace,
+		Name:      name,
+		Values:    make(map[string]string),
+	}
+
+	eventRepoRef, err := image.Parse(repo.Name)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error":           err,
+			"repository_name": repo.Name,
+		}).Error("provider.helm: failed to parse event repository name")
+		return
+	}
+
+	// getting configuration
+	vals, err := values(chart, config)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Error("provider.helm: failed to get values.yaml for release")
+		return
+	}
+
+	keelCfg, err := getKeelConfig(vals)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Error("provider.helm: failed to get keel configuration for release")
+		// ignoring this release, no keel config found
+		return plan, false, nil
+	}
+
+	if keelCfg.Policy != types.PolicyTypeForce {
+		// policy is not force, ignoring release
+		return plan, false, nil
+	}
+
+	// checking for impacted images
+	for _, imageDetails := range keelCfg.Images {
+		fmt.Println(imageDetails.TagPath)
+
+		imageRef, err := parseImage(vals, &imageDetails)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"error":           err,
+				"repository_name": imageDetails.RepositoryPath,
+				"repository_tag":  imageDetails.TagPath,
+			}).Error("provider.helm: failed to parse image")
+			continue
+		}
+
+		fmt.Println(imageRef.Repository())
+
+		if imageRef.Repository() != eventRepoRef.Repository() {
+			log.WithFields(log.Fields{
+				"parsed_image_name": imageRef.Remote(),
+				"target_image_name": repo.Name,
+			}).Info("provider.helm: images do not match, ignoring")
+			continue
+		}
+
+		path, value := getUnversionedPlanValues(repo.Tag, imageRef, &imageDetails)
+		plan.Values[path] = value
+		shouldUpdateRelease = true
+	}
+
+	return plan, shouldUpdateRelease, nil
+}
