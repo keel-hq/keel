1da90e54ad505d91203d08ef2b1c195038e81ee8
Karolis Rusenas <karolis.rusenas@gmail.com>
2017-07-28 22:54:21 +0100

    adding secrets method

--- util/testing/testing.go
+++ util/testing/testing.go
@@ -14,6 +14,8 @@ type FakeK8sImplementer struct {
 
 	// stores value of an updated deployment
 	Updated *v1beta1.Deployment
+
+	AvailableSecret *v1.Secret
 }
 
 func (i *FakeK8sImplementer) Namespaces() (*v1.NamespaceList, error) {
@@ -31,4 +33,8 @@ func (i *FakeK8sImplementer) Deployments(namespace string) (*v1beta1.DeploymentL
 func (i *FakeK8sImplementer) Update(deployment *v1beta1.Deployment) error {
 	i.Updated = deployment
 	return nil
+}
+
+func (i *FakeK8sImplementer) Secret(namespace, name string) (*v1.Secret, error) {
+	return i.AvailableSecret, nil
 }
