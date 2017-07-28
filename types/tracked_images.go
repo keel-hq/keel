556294e87fafa27053da05e4cad9378c3c0601ba
Karolis Rusenas <karolis.rusenas@gmail.com>
2017-07-28 22:55:02 +0100

    splitting

--- types/tracked_images.go
+++ types/tracked_images.go
@@ -4,9 +4,15 @@ import (
 	"github.com/rusenask/keel/util/image"
 )
 
+type Credentials struct {
+	Username, Password string
+}
+
 type TrackedImage struct {
 	Image        *image.Reference
 	Trigger      TriggerType
 	PollSchedule string
 	Provider     string
+	Namespace    string
+	Secrets      []string
 }
