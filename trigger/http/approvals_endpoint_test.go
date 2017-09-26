3716debb17cc4e1feaeb8325f2068cedaa5cf6d6
Karolis Rusenas <karolis.rusenas@gmail.com>
2017-09-26 15:29:00 +0100

    delete test

--- trigger/http/approvals_endpoint_test.go
+++ trigger/http/approvals_endpoint_test.go
@@ -17,7 +17,7 @@ import (
 func TestListApprovals(t *testing.T) {
 
 	fp := &fakeProvider{}
-	mem := memory.NewMemoryCache(100*time.Millisecond, 100*time.Millisecond, 10*time.Millisecond)
+	mem := memory.NewMemoryCache(100*time.Second, 100*time.Second, 10*time.Second)
 	am := approvals.New(mem, codecs.DefaultSerializer())
 	providers := provider.New([]provider.Provider{fp}, am)
 	srv := NewTriggerServer(&Opts{Providers: providers, ApprovalManager: am})
@@ -69,4 +69,45 @@ func TestListApprovals(t *testing.T) {
 	if approvals[0].CurrentVersion != "1.0.0" {
 		t.Errorf("unexpected current version: %s", approvals[0].CurrentVersion)
 	}
+}
+
+func TestDeleteApproval(t *testing.T) {
+	fp := &fakeProvider{}
+	mem := memory.NewMemoryCache(100*time.Second, 100*time.Second, 10*time.Second)
+	am := approvals.New(mem, codecs.DefaultSerializer())
+	providers := provider.New([]provider.Provider{fp}, am)
+	srv := NewTriggerServer(&Opts{Providers: providers, ApprovalManager: am})
+	srv.registerRoutes(srv.router)
+
+	err := am.Create(&types.Approval{
+		Identifier:     "12345",
+		VotesRequired:  5,
+		NewVersion:     "2.0.0",
+		CurrentVersion: "1.0.0",
+	})
+
+	if err != nil {
+		t.Fatalf("failed to create approval: %s", err)
+	}
+
+	// listing
+	req, err := http.NewRequest("DELETE", "/v1/approvals/12345", nil)
+	if err != nil {
+		t.Fatalf("failed to create req: %s", err)
+	}
+
+	rec := httptest.NewRecorder()
+
+	srv.router.ServeHTTP(rec, req)
+	if rec.Code != 200 {
+		t.Errorf("unexpected status code: %d", rec.Code)
+
+		t.Log(rec.Body.String())
+	}
+
+	_, err = am.Get("12345")
+	if err == nil {
+		t.Errorf("expected approval to be deleted")
+	}
+
 }
