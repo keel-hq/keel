4d067cd647f1d1126f2879c8d73083fbb6ce33f4
Igor Komlew <igor.komlew@innovation.hrs.com>
2017-12-14 12:12:23 +0100

    added support for hipchat notifications

--- constants/constants.go
+++ constants/constants.go
@@ -11,10 +11,14 @@ const WebhookEndpointEnv = "WEBHOOK_ENDPOINT"
 
 // slack bot/token
 const (
-	EnvSlackToken    = "SLACK_TOKEN"
-	EnvSlackBotName  = "SLACK_BOT_NAME"
-	EnvSlackChannels = "SLACK_CHANNELS"
+	EnvSlackToken            = "SLACK_TOKEN"
+	EnvSlackBotName          = "SLACK_BOT_NAME"
+	EnvSlackChannels         = "SLACK_CHANNELS"
 	EnvSlackApprovalsChannel = "SLACK_APPROVALS_CHANNEL"
+
+	EnvHipchatToken    = "HIPCHAT_TOKEN"
+	EnvHipchatBotName  = "HIPCHAT_BOT_NAME"
+	EnvHipchatChannels = "HIPCHAT_CHANNELS"
 )
 
 // EnvNotificationLevel - minimum level for notifications, defaults to info
---
+++ extension/notification/hipchat/hipchat.go
@@ -0,0 +1,96 @@
+package hipchat
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/tbruyelle/hipchat-go/hipchat"
+
+	"github.com/keel-hq/keel/constants"
+	"github.com/keel-hq/keel/extension/notification"
+	"github.com/keel-hq/keel/types"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+type sender struct {
+	hipchatClient *hipchat.Client
+	channels      []string
+	botName       string
+}
+
+func init() {
+	notification.RegisterSender("hipchat", &sender{})
+}
+
+func (s *sender) Configure(config *notification.Config) (bool, error) {
+	var token string
+
+	if os.Getenv(constants.EnvHipchatToken) != "" {
+		token = os.Getenv(constants.EnvHipchatToken)
+	} else {
+		return false, nil
+	}
+	if os.Getenv(constants.EnvHipchatBotName) != "" {
+		s.botName = os.Getenv(constants.EnvHipchatBotName)
+	} else {
+		s.botName = "keel"
+	}
+
+	if os.Getenv(constants.EnvHipchatChannels) != "" {
+		channels := os.Getenv(constants.EnvHipchatChannels)
+		s.channels = strings.Split(channels, ",")
+	} else {
+		s.channels = []string{"general"}
+	}
+
+	s.hipchatClient = hipchat.NewClient(token)
+
+	log.WithFields(log.Fields{
+		"name":     "hipchat",
+		"channels": s.channels,
+	}).Info("extension.notification.hipchat: sender configured")
+
+	return true, nil
+}
+
+func (s *sender) Send(event types.EventNotification) error {
+	msg := fmt.Sprintf("<b>%s</b><br>%s", event.Type.String(), event.Message)
+
+	notification := &hipchat.NotificationRequest{
+		Color:   getHipchatColor(event.Level.String()),
+		Message: msg,
+		Notify:  true,
+		From:    s.botName,
+	}
+
+	for _, channel := range s.channels {
+		_, err := s.hipchatClient.Room.Notification(channel, notification)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"error":   err,
+				"channel": channel,
+			}).Error("extension.notification.hipchat: failed to send notification")
+		}
+	}
+
+	return nil
+}
+
+func getHipchatColor(eventLevel string) hipchat.Color {
+	switch eventLevel {
+	case "error":
+		return "red"
+	case "info":
+		return "gray"
+	case "success":
+		return "green"
+	case "fatal":
+		return "purple"
+	case "warn":
+		return "yellow"
+	default:
+		return "gray"
+	}
+}
