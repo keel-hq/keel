04261f80f2aa66876e78b6ca4608c7541ccfdebe
Igor Komlew <igor.komlew@innovation.hrs.com>
2017-12-20 11:54:22 +0100

    WIP: first step for hipchat integration

--- constants/constants.go
+++ constants/constants.go
@@ -19,6 +19,11 @@ const (
 	EnvHipchatToken    = "HIPCHAT_TOKEN"
 	EnvHipchatBotName  = "HIPCHAT_BOT_NAME"
 	EnvHipchatChannels = "HIPCHAT_CHANNELS"
+
+	EnvHipchatApprovalsChannel  = "HIPCHAT_APPROVALS_CHANNEL"
+	EnvHipchatApprovalsUserName = "HIPCHAT_APPROVALS_USER_NAME"
+	EnvHipchatApprovalsBotName  = "HIPCHAT_APPROVALS_BOT_NAME"
+	EnvHipchatApprovalsPasswort = "HIPCHAT_APPROVALS_PASSWORT"
 )
 
 // EnvNotificationLevel - minimum level for notifications, defaults to info
