8d05fe1a4b86eae21bb570eb11b7f7cc2e474c01
Karolis Rusenas <karolis.rusenas@gmail.com>
2018-04-17 08:51:59 +0100

    cache control

---
+++ internal/k8s/translator.go
@@ -0,0 +1,44 @@
+package k8s
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+type Translator struct {
+	logrus.FieldLogger
+
+	GenericResourceCache
+
+	KeelSelector string
+}
+
+func (t *Translator) OnAdd(obj interface{}) {
+	gr, err := NewGenericResource(obj)
+	if err != nil {
+		t.Errorf("OnAdd failed to add resource %T: %#v", obj, obj)
+		return
+	}
+	t.Infof("added %s %s", gr.Kind(), gr.Name)
+	t.GenericResourceCache.Add(gr)
+}
+
+func (t *Translator) OnUpdate(oldObj, newObj interface{}) {
+	gr, err := NewGenericResource(newObj)
+	if err != nil {
+		t.Errorf("OnUpdate failed to update resource %T: %#v", newObj, newObj)
+		return
+	}
+
+	t.Infof("updated %s %s", gr.Kind(), gr.Name)
+	t.GenericResourceCache.Add(gr)
+}
+
+func (t *Translator) OnDelete(obj interface{}) {
+	gr, err := NewGenericResource(obj)
+	if err != nil {
+		t.Errorf("OnDelete failed to delete resource %T: %#v", obj, obj)
+		return
+	}
+	t.Infof("deleted %s %s", gr.Kind(), gr.Name)
+	t.GenericResourceCache.Remove(gr.GetIdentifier())
+}
