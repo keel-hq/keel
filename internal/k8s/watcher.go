8787ec285a1640ccf4aab271475058d698b8b9b1
Karolis Rusenas <karolis.rusenas@gmail.com>
2018-04-17 08:50:12 +0100

    resource watchers

---
+++ internal/k8s/watcher.go
@@ -0,0 +1,119 @@
+package k8s
+
+import (
+	"time"
+
+	"github.com/keel-hq/keel/internal/workgroup"
+	"github.com/sirupsen/logrus"
+
+	apps_v1 "k8s.io/api/apps/v1"
+	"k8s.io/api/core/v1"
+
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+)
+
+// WatchDeployments creates a SharedInformer for apps/v1.Deployments and registers it with g.
+func WatchDeployments(g *workgroup.Group, client *kubernetes.Clientset, log logrus.FieldLogger, rs ...cache.ResourceEventHandler) {
+	watch(g, client.AppsV1().RESTClient(), log, "deployments", new(apps_v1.Deployment), rs...)
+}
+
+// WatchStatefulSets creates a SharedInformer for apps/v1.StatefulSet and registers it with g.
+func WatchStatefulSets(g *workgroup.Group, client *kubernetes.Clientset, log logrus.FieldLogger, rs ...cache.ResourceEventHandler) {
+	watch(g, client.AppsV1().RESTClient(), log, "statefulsets", new(apps_v1.StatefulSet), rs...)
+}
+
+// WatchDaemonSets creates a SharedInformer for apps/v1.DaemonSet and registers it with g.
+func WatchDaemonSets(g *workgroup.Group, client *kubernetes.Clientset, log logrus.FieldLogger, rs ...cache.ResourceEventHandler) {
+	watch(g, client.AppsV1().RESTClient(), log, "daemonsets", new(apps_v1.DaemonSet), rs...)
+}
+
+func watch(g *workgroup.Group, c cache.Getter, log logrus.FieldLogger, resource string, objType runtime.Object, rs ...cache.ResourceEventHandler) {
+	lw := cache.NewListWatchFromClient(c, resource, v1.NamespaceAll, fields.Everything())
+	sw := cache.NewSharedInformer(lw, objType, 30*time.Minute)
+	for _, r := range rs {
+		sw.AddEventHandler(r)
+	}
+	g.Add(func(stop <-chan struct{}) {
+		log := log.WithField("resource", resource)
+		log.Println("started")
+		defer log.Println("stopped")
+		sw.Run(stop)
+	})
+}
+
+type buffer struct {
+	ev chan interface{}
+	logrus.StdLogger
+	rh cache.ResourceEventHandler
+}
+
+type addEvent struct {
+	obj interface{}
+}
+
+type updateEvent struct {
+	oldObj, newObj interface{}
+}
+
+type deleteEvent struct {
+	obj interface{}
+}
+
+// NewBuffer returns a ResourceEventHandler which buffers and serialises ResourceEventHandler events.
+func NewBuffer(g *workgroup.Group, rh cache.ResourceEventHandler, log logrus.FieldLogger, size int) cache.ResourceEventHandler {
+	buf := &buffer{
+		ev:        make(chan interface{}, size),
+		StdLogger: log.WithField("context", "buffer"),
+		rh:        rh,
+	}
+	g.Add(buf.loop)
+	return buf
+}
+
+func (b *buffer) loop(stop <-chan struct{}) {
+	b.Println("started")
+	defer b.Println("stopped")
+
+	for {
+		select {
+		case ev := <-b.ev:
+			switch ev := ev.(type) {
+			case *addEvent:
+				b.rh.OnAdd(ev.obj)
+			case *updateEvent:
+				b.rh.OnUpdate(ev.oldObj, ev.newObj)
+			case *deleteEvent:
+				b.rh.OnDelete(ev.obj)
+			default:
+				b.Printf("unhandled event type: %T: %v", ev, ev)
+			}
+		case <-stop:			
+			return
+		}
+	}
+}
+
+func (b *buffer) OnAdd(obj interface{}) {
+	b.send(&addEvent{obj})
+}
+
+func (b *buffer) OnUpdate(oldObj, newObj interface{}) {
+	b.send(&updateEvent{oldObj, newObj})
+}
+
+func (b *buffer) OnDelete(obj interface{}) {
+	b.send(&deleteEvent{obj})
+}
+
+func (b *buffer) send(ev interface{}) {
+	select {
+	case b.ev <- ev:
+		// all good
+	default:
+		b.Printf("event channel is full, len: %v, cap: %v", len(b.ev), cap(b.ev))
+		b.ev <- ev
+	}
+}
