701f417741bcabd135df0aac827dca74ec61fd58
Karolis Rusenas <karolis.rusenas@gmail.com>
2018-09-04 21:25:44 +0100

    policy parse tests

--- internal/policy/policy_test.go
+++ internal/policy/policy_test.go
@@ -1,6 +1,7 @@
 package policy
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/keel-hq/keel/types"
@@ -43,6 +44,49 @@ func Test_getPolicyFromLabels(t *testing.T) {
 			}
 			if got1 != tt.want1 {
 				t.Errorf("getPolicyFromLabels() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func mustParseGlob(g string) *GlobPolicy {
+	glb, err := NewGlobPolicy(g)
+	if err != nil {
+		panic(err)
+	}
+	return glb
+}
+
+func TestGetPolicy(t *testing.T) {
+	type args struct {
+		policyName string
+		options    *Options
+	}
+	tests := []struct {
+		name string
+		args args
+		want Policy
+	}{
+		{
+			name: "patch",
+			args: args{policyName: "patch", options: &Options{}},
+			want: NewSemverPolicy(SemverPolicyTypePatch),
+		},
+		{
+			name: "glob:foo-*",
+			args: args{policyName: "glob:foo-*", options: &Options{}},
+			want: mustParseGlob("glob:foo-*"),
+		},
+		{
+			name: "force match",
+			args: args{policyName: "force", options: &Options{MatchTag: true}},
+			want: NewForcePolicy(true),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetPolicy(tt.args.policyName, tt.args.options); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetPolicy() = %v, want %v", got, tt.want)
 			}
 		})
 	}
