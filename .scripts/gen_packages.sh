4e7d30b376a6817fd2aab658ab43ed92ff0aad8f
rimas <rmocius@gmail.com>
2018-11-23 12:54:57 +0000

    GCP cloud build integration and chart lint and install test

---
+++ .scripts/gen_packages.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+echo "Installing curl"
+apt update
+apt install curl -y
+
+echo "Installing helm"
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
+helm init -c
+
+echo "Packaging charts from source code"
+mkdir -p temp
+for d in stable/*
+do
+ # shellcheck disable=SC2039
+ if [[ -d $d ]]
+ then
+    # Will generate a helm package per chart in a folder
+    echo "$d"
+    helm package "$d"
+    # shellcheck disable=SC2035
+    mv *.tgz temp/
+  fi
+done
