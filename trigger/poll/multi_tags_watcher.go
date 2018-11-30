f35c10ab7f94e3f75a078e0e6cf4b3d4609b1da1
Karolis Rusenas <karolis.rusenas@gmail.com>
2018-11-30 22:39:49 +0000

    simplifying polling

--- trigger/poll/multi_tags_watcher.go
+++ trigger/poll/multi_tags_watcher.go
@@ -1,12 +1,10 @@
 package poll
 
 import (
-	"fmt"
-	"strings"
-
-	"github.com/Masterminds/semver"
+	"sort"
 
 	"github.com/keel-hq/keel/extension/credentialshelper"
+	"github.com/keel-hq/keel/internal/policy"
 	"github.com/keel-hq/keel/provider"
 	"github.com/keel-hq/keel/registry"
 	"github.com/keel-hq/keel/types"
@@ -23,7 +21,7 @@ type WatchRepositoryTagsJob struct {
 	registryClient registry.Client
 	details        *watchDetails
 
-	latests map[string]string // a map of prerelease tags and their corresponding latest versions
+	// latests map[string]string // a map of prerelease tags and their corresponding latest versions
 }
 
 // NewWatchRepositoryTagsJob - new tags watcher job
@@ -32,7 +30,7 @@ func NewWatchRepositoryTagsJob(providers provider.Providers, registryClient regi
 		providers:      providers,
 		registryClient: registryClient,
 		details:        details,
-		latests:        details.trackedImage.SemverPreReleaseTags,
+		// latests:        details.trackedImage.SemverPreReleaseTags,
 	}
 }
 
@@ -83,104 +81,116 @@ func (j *WatchRepositoryTagsJob) Run() {
 	}
 }
 
-func (j *WatchRepositoryTagsJob) processTags(tags []string) error {
-	var errors []string
-
-	log.WithFields(log.Fields{
-		"latest":   j.details.latest,
-		"all_tags": tags,
-	}).Debug("trigger.poll.WatchRepositoryTagsJob: checking for new version")
-	latestVersion, newAvailable, err := version.NewAvailable(j.details.latest, tags, false)
+func (j *WatchRepositoryTagsJob) computeEvents(tags []string) ([]types.Event, error) {
+	trackedImages, err := j.providers.TrackedImages()
 	if err != nil {
-		errors = append(errors, fmt.Sprintf("new available version func returned an error: %s", err))
+		return nil, err
 	}
 
-	if newAvailable {
-		log.Debugf("new tag '%s' available, submitting event and assigning it to be the latest", latestVersion)
-		// updating current latest
-		j.details.latest = latestVersion
-		event := types.Event{
-			Repository: types.Repository{
-				Name: j.details.trackedImage.Image.Repository(),
-				Tag:  latestVersion,
-			},
-			TriggerName: types.TriggerTypePoll.String(),
-		}
-		log.WithFields(log.Fields{
-			"repository": j.details.trackedImage.Image.Repository(),
-			"new_tag":    latestVersion,
-		}).Debug("trigger.poll.WatchRepositoryTagsJob: submiting event to providers")
-		err := j.providers.Submit(event)
-		if err != nil {
-			log.WithFields(log.Fields{
-				"repository": j.details.trackedImage.Image.Repository(),
-				"new_tag":    latestVersion,
-				"error":      err,
-			}).Error("trigger.poll.WatchRepositoryTagsJob: error while submitting an event")
-		}
-	}
+	events := []types.Event{}
 
-	log.Debugf("trigger.poll.WatchRepositoryTagsJob: processing tags %s", tags)
-	for _, tag := range tags {
-		sv, err := semver.NewVersion(tag)
-		if err != nil {
-			continue
-		}
-		if sv.Prerelease() == "" {
-			continue
-		}
-		trackedVersion, ok := j.details.trackedImage.SemverPreReleaseTags[sv.Prerelease()]
+	// collapse removes all non-semver tags and only takes
+	// the highest versions of each prerelease + the main version that doesn't have
+	// any prereleases
+	tags = collapse(tags)
 
-		log.WithFields(log.Fields{
-			"all":         tags,
-			"pre_release": sv.Prerelease(),
-			"tracked":     trackedVersion,
-			"tracked_all": j.details.trackedImage.SemverPreReleaseTags,
-		}).Debug("looking for a pre-release")
-
-		if ok {
-			latestVersion, newAvailable, err := version.NewAvailable(trackedVersion, tags, true)
+	for _, trackedImage := range getRelatedTrackedImages(j.details.trackedImage, trackedImages) {
+		// matches, going through tags
+		for _, tag := range tags {
+			update, err := trackedImage.Policy.ShouldUpdate(trackedImage.Image.Tag(), tag)
 			if err != nil {
-				errors = append(errors, fmt.Sprintf("new available version func for tag %s returned an error: %s", trackedVersion, err))
 				continue
 			}
-
-			if newAvailable {
-				// log.Debugf("new tag with prerelease '%s' available", latestVersion)
-				// updating current latest
-				// j.details.latest = latestVersion
-				j.details.trackedImage.SemverPreReleaseTags[sv.Prerelease()] = latestVersion
-
+			if update && !exists(tag, events) {
 				event := types.Event{
 					Repository: types.Repository{
 						Name: j.details.trackedImage.Image.Repository(),
-						Tag:  latestVersion,
+						Tag:  tag,
 					},
 					TriggerName: types.TriggerTypePoll.String(),
 				}
-				log.WithFields(log.Fields{
-					"repository": j.details.trackedImage.Image.Repository(),
-					"new_tag":    latestVersion,
-				}).Debug("trigger.poll.WatchRepositoryTagsJob: submiting event to providers")
-				err := j.providers.Submit(event)
-				if err != nil {
-					log.WithFields(log.Fields{
-						"repository": j.details.trackedImage.Image.Repository(),
-						"new_tag":    latestVersion,
-						"error":      err,
-					}).Error("trigger.poll.WatchRepositoryTagsJob: error while submitting an event")
-				}
+				events = append(events, event)
 			}
 
-			if err != nil {
-				errors = append(errors, fmt.Sprintf("new available version func returned an error: %s", err))
-			}
 		}
-		// nothing to do
+
 	}
 
-	if len(errors) > 0 {
-		return fmt.Errorf("errors while processing repository tags: %s", strings.Join(errors, ","))
+	return events, nil
+}
+
+func exists(tag string, events []types.Event) bool {
+	for _, e := range events {
+		if tag == e.Repository.Tag {
+			return true
+		}
+	}
+	return false
+}
+
+// collapse gets latest available tags for main version and pre-releases
+// example:
+// [1.0.0, 1.5.0, 1.3.0-dev, 1.4.5-dev] would become [1.5.0, 1.4.5-dev]
+func collapse(tags []string) []string {
+	r := map[string]string{}
+	p := policy.NewSemverPolicy(policy.SemverPolicyTypeAll)
+	for _, t := range tags {
+		v, err := version.GetVersion(t)
+		// v, err := semver.NewVersion(tag)
+		if err != nil {
+			continue
+		}
+		stored, ok := r[v.PreRelease]
+		if !ok {
+			r[v.PreRelease] = t
+			continue
+		}
+		higher, err := p.ShouldUpdate(stored, t)
+		if err != nil {
+			continue
+		}
+		if higher {
+			r[v.PreRelease] = t
+		}
+	}
+
+	result := []string{}
+	for _, tag := range r {
+		result = append(result, tag)
+	}
+
+	// always sort, for test purposes
+	sort.Strings(result)
+
+	return result
+}
+
+func getRelatedTrackedImages(ours *types.TrackedImage, all []*types.TrackedImage) []*types.TrackedImage {
+	b := all[:0]
+	for _, x := range all {
+		if x.Image.Repository() == ours.Image.Repository() {
+			b = append(b, x)
+		}
+	}
+
+	return b
+}
+
+func (j *WatchRepositoryTagsJob) processTags(tags []string) error {
+
+	events, err := j.computeEvents(tags)
+	if err != nil {
+		return err
+	}
+	for _, e := range events {
+		err = j.providers.Submit(e)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"repository": j.details.trackedImage.Image.Repository(),
+				"new_tag":    e.Repository.Tag,
+				"error":      err,
+			}).Error("trigger.poll.WatchRepositoryTagsJob: error while submitting an event")
+		}
 	}
 	return nil
 }
