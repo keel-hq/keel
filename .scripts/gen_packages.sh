1af541179baf75391bd801559322ff66b4433825
Leif Segen <lsegen@herzog.com>
2020-09-08 13:17:00 -0500

    Update gen_packages.sh
    Update shell check reference

--- .scripts/gen_packages.sh
+++ .scripts/gen_packages.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 echo "Installing curl"
@@ -13,7 +13,7 @@ echo "Packaging charts from source code"
 mkdir -p temp
 for d in chart/*
 do
- # shellcheck disable=SC2039
+ # shellcheck disable=SC3010
  if [[ -d $d ]]
  then
     # Will generate a helm package per chart in a folder
