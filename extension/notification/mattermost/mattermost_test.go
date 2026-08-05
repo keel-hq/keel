9163da76e54a7bae2d802deb60d5422220113eb1
karolis <karolis@rusenas.dev>
2026-08-05 15:22:25 +0400

    fix(notifications): accept all successful webhook statuses
    Spec-Ref: helix-specs@7647bd55:000265_accept-valid-http-202

--- extension/notification/mattermost/mattermost_test.go
+++ extension/notification/mattermost/mattermost_test.go
@@ -1,68 +1,16 @@
-package webhook
+package mattermost
 
 import (
-	"io"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"sync/atomic"
 	"testing"
-	"time"
 
 	"github.com/keel-hq/keel/types"
 )
 
-func TestWebhookRequest(t *testing.T) {
-	currentTime := time.Now()
-	handler := func(resp http.ResponseWriter, req *http.Request) {
-		body, err := io.ReadAll(req.Body)
-		if err != nil {
-			t.Errorf("failed to parse body: %s", err)
-		}
-
-		bodyStr := string(body)
-
-		if !strings.Contains(bodyStr, types.NotificationPreDeploymentUpdate.String()) {
-			t.Errorf("missing deployment type")
-		}
-
-		if !strings.Contains(bodyStr, "debug") {
-			t.Errorf("missing level")
-		}
-
-		if !strings.Contains(bodyStr, "update deployment") {
-			t.Errorf("missing name")
-		}
-		if !strings.Contains(bodyStr, "message here") {
-			t.Errorf("missing message")
-		}
-
-		t.Log(bodyStr)
-
-	}
-
-	// create test server with handler
-	ts := httptest.NewServer(http.HandlerFunc(handler))
-	defer ts.Close()
-
-	s := &sender{
-		endpoint: ts.URL,
-		client:   &http.Client{},
-	}
-
-	err := s.Send(types.EventNotification{
-		Name:      "update deployment",
-		Message:   "message here",
-		CreatedAt: currentTime,
-		Type:      types.NotificationPreDeploymentUpdate,
-		Level:     types.LevelDebug,
-	})
-	if err != nil {
-		t.Fatalf("Send() error = %v", err)
-	}
-}
-
-func TestWebhookResponseStatuses(t *testing.T) {
+func TestMattermostResponseStatuses(t *testing.T) {
 	tests := []struct {
 		name       string
 		statusCode int
@@ -94,6 +42,7 @@ func TestWebhookResponseStatuses(t *testing.T) {
 
 			s := &sender{
 				endpoint: server.URL,
+				name:     "keel",
 				client:   &http.Client{CheckRedirect: rejectRedirect},
 			}
 
